1d559f3bf001428841706bf5aee65d5af0d75ef4
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-05-28 15:45:59 +0200

    More advanced alternate browser launcher
    If the first alt browser (surf+tabbed) is already running, run the
    second alt browser (Firefox).

--- xmonad.hs
+++ xmonad.hs
@@ -285,7 +285,9 @@ myKeymap =
   , ("M-S-m"         , mediaPlayer)
   , ("M-<Escape>"    , mediaPlayer)
   , ("C-M-S-o"       , spawnHere "firefox -P default")
-  , ("C-M-S-M5-o"    , spawnHere "firefox -P Business")
+  , ("C-M-S-M5-o"    , ifWindows (className =? "tabbed" <&&> resource =? "surf")
+                                 (\_ -> spawnHere "firefox -P Business")
+                                 (spawnHere "tabbed -n surf surf -e"))
   ]
   ++
   [ (key, maximizeWindow)
